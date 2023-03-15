fe8e75811c0686b32f13d7b92d1c43b21c6580c7
qinyukun <qinyukun@risound-china.com>
2023-03-15 11:16:33 +0800

    add audio psk read and motify code

--- UI_data.py
+++ UI_data.py
@@ -1,5 +1,5 @@
 # 先导入生成的Ui界面模块
-from PyQt5.QtWidgets import QMainWindow, QAction
+from PyQt5.QtWidgets import QMainWindow, QAction,QLabel
 
 from qcc_ui import Ui_MainWindow
 from TestEngineAPI import TestEngine
@@ -15,7 +15,7 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         exe_path = os.path.join(_DIRNAME, "x86", "TestEngine.dll")
         self.myDll = TestEngine(exe_path)
         retval, versionStr = self.myDll.teGetVersion()
-        self.statusBar.showMessage("DLL version:"+versionStr)
+        #self.statusBar.showMessage("DLL version:"+versionStr)
 
         #trans_list = trans.split(',')
         self.port_info = {}
@@ -26,7 +26,15 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         self.port_dataRate = 0 #Defines the baud rate to be used (for UART connections only).
         self.port_retryTimeOut =5000 #5000ms
         self.port_usbTimeout = 1000 #1000ms
+        self.statusBar_label = QLabel()
+        self.statusBar.addWidget(self.statusBar_label)
+        #self.statusBar_label.setText("DLL version:"+versionStr)
+        self.statusBar_label.setText('<font color="blue">DLL version:{st1}</font>'.format(st1=versionStr))
 
+        self.statusBar_label_permanent  = QLabel()
+        self.statusBar_label_permanent.setText("<a href=\"https://github.com/xiaokun9/qcc_tool/wiki\">github wiki</a>")
+        self.statusBar_label_permanent.setOpenExternalLinks(True)
+        self.statusBar.addPermanentWidget(self.statusBar_label_permanent)
 
         self.comboBox_1.activated.connect(self.port_status_change1)
 
@@ -45,6 +53,8 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         self.refresh_port_action.triggered.connect(self.refresh_port)
         self.refresh_port_action.setText('刷新端口')
         self.menubar.addAction(self.refresh_port_action)
+        self.refresh_port_action.setStatusTip('刷新端口')
+
         #Apps keys
         self.refresh_app_ps_action = QAction(self)
         self.refresh_app_ps_action.setCheckable(False)
@@ -52,6 +62,8 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         self.refresh_app_ps_action.triggered.connect(self.refresh_app_psk)
         self.refresh_app_ps_action.setText('读取app psk')
         self.menubar.addAction(self.refresh_app_ps_action)
+        self.refresh_app_ps_action.setStatusTip('读取app psk')
+
         #Audio keys
         self.refresh_audio_psk_action = QAction(self)
         self.refresh_audio_psk_action.setCheckable(False)
@@ -59,6 +71,8 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         self.refresh_audio_psk_action.triggered.connect(self.refresh_audio_psk)
         self.refresh_audio_psk_action.setText('读取audio psk')
         self.menubar.addAction(self.refresh_audio_psk_action)
+        self.refresh_audio_psk_action.setStatusTip('读取audio psk')
+
     def refresh_port(self,checked):
         #print(self.sender().text())
         #print(checked)
@@ -87,7 +101,7 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
                 pass
             #self.port_info[ports_list[index]] = trans_list[index]
             #print(ports_list[index].replace(' ','').replace('(','').replace(')',''))
-
+        self.statusBar_label.setText('<font color="blue">端口刷新完成</font>')
     def refresh_app_psk(self, checked):
         #print("refresh_app_psk")
         self.app_psk_id.clear()
@@ -139,12 +153,14 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         #print(self.handle1)
         if self.handle1 == self.myDll.TE_INVALID_HANDLE_VALUE:
             #print("port1_open fail ")
-            self.statusBar.showMessage(self.cur_port1 + " open fail,handle:" + str(self.handle1))
+            #self.statusBar.showMessage(self.cur_port1 + " open fail,handle:" + str(self.handle1))
+            self.statusBar_label.setText('<font color="red">{port}open fail,handle:{hand}</font>'.format(port=self.cur_port1,hand=self.handle1))
             #print(port_device + " open fail,handle:"+ self.handle1)
             #self.textBrowser.setText(port_device + " open fail,handle:" + str(self.handle1))
         else:
             #print("port1_open succeed")
             self.statusBar.showMessage(self.cur_port1 + " open succeed,handle:" + str(self.handle1))
+            self.statusBar_label.setText('<font color="blue">{port}open succeed,handle:{hand}</font>'.format(port=self.cur_port1,hand=self.handle1))
             #print(port_device + " open succeed,handle:"+ self.handle1)
             #self.textBrowser.setText(port_device + " open succeed,handle:" + str(self.handle1))
 
@@ -157,17 +173,21 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
             #return
         retval = self.myDll.teConfigCacheInit(self.handle1, configDb='config\hydracore_config.sdb:QCC517X_CONFIG')
         if retval == self.myDll.TE_OK:
-            self.statusBar.showMessage("ConfigCacheInit Succeed")
+            #self.statusBar.showMessage("ConfigCacheInit Succeed")
+            self.statusBar_label.setText('<font color="blue">ConfigCacheInit Succeed</font>')
         else:
-            self.statusBar.showMessage("ConfigCacheInit Fail,please retry!!!")
+            #self.statusBar.showMessage("ConfigCacheInit Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">ConfigCacheInit Fail,please retry!!!</font>')
             return
         #print(retval)
 
         retval = self.myDll.teConfigCacheRead(self.handle1, None, 0) #None:read for device
         if retval == self.myDll.TE_OK:
-            self.statusBar.showMessage("ConfigCacheRead Succeed")
+            #self.statusBar.showMessage("ConfigCacheRead Succeed")
+            self.statusBar_label.setText('<font color="blue">ConfigCacheRead Succeed</font>')
         else:
-            self.statusBar.showMessage("ConfigCacheRead Fail,please retry!!!")
+            #self.statusBar.showMessage("ConfigCacheRead Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">ConfigCacheRead Fail,please retry!!!</font>')
             return
         #print(retval)
         retval, value, maxLen = self.myDll.teConfigCacheReadItem(self.handle1, key='bt2:BD_ADDRESS', maxLen=100)
@@ -188,9 +208,11 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         #print(retval, value, maxLen)
         self.lineEdit_1_name.setText(value)#bt name
         if retval != self.myDll.TE_OK:
-            self.statusBar.showMessage("Cache Read Fail,please retry!!!")
+            #self.statusBar.showMessage("Cache Read Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">Cache Read Fail,please retry!!!</font>')
         else:
-            self.statusBar.showMessage("Cache Read Succeed")
+            #self.statusBar.showMessage("Cache Read Succeed")
+            self.statusBar_label.setText('<font color="blue">Cache Read Succeed</font>')
     def port_write_addr_name_1(self):
         #step 1 ->teConfigCacheInit
         if self.lineEdit_1_name.text() == '' or self.lineEdit_1_BD_addr.text() == '':
@@ -199,16 +221,20 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
             return
         retval = self.myDll.teConfigCacheInit(self.handle1, configDb='config\hydracore_config.sdb:QCC517X_CONFIG')
         if retval == self.myDll.TE_OK:
-            self.statusBar.showMessage("ConfigCacheInit Succeed")
+            #self.statusBar.showMessage("ConfigCacheInit Succeed")
+            self.statusBar_label.setText('<font color="blue">ConfigCacheInit Succeed</font>')
         else:
-            self.statusBar.showMessage("ConfigCacheInit Fail,please retry!!!")
+            #self.statusBar.showMessage("ConfigCacheInit Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">ConfigCacheInit Fail,please retry!!!</font>')
             return
         #step 2 ->teConfigCacheRead
         retval = self.myDll.teConfigCacheRead(self.handle1, None, 0) #None:read for device
         if retval == self.myDll.TE_OK:
-            self.statusBar.showMessage("ConfigCacheRead Succeed")
+            #self.statusBar.showMessage("ConfigCacheRead Succeed")
+            self.statusBar_label.setText('<font color="blue">ConfigCacheRead Succeed</font>')
         else:
-            self.statusBar.showMessage("ConfigCacheRead Fail,please retry!!!")
+            #self.statusBar.showMessage("ConfigCacheRead Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">ConfigCacheRead Fail,please retry!!!</font>')
             return
         #step 3 ->teConfigCacheWriteItem (addr)
 
@@ -219,18 +245,22 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         str_w='[{str1}]'.format(str1=str)
         retval = self.myDll.teConfigCacheWriteItem(self.handle1, 'bt2:BD_ADDRESS', str_w)
         if retval != self.myDll.TE_OK:
-            self.statusBar.showMessage("BD Addr Write Cache Fail,please retry!!!")
+            #self.statusBar.showMessage("BD Addr Write Cache Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">BD Addr Write Cache Fail,please retry!!!</font>')
         #step 4 ->teConfigCacheWriteItem (name)
         str_w='"{str1}"'.format(str1=self.lineEdit_1_name.text())
         retval = self.myDll.teConfigCacheWriteItem(self.handle1, 'app5:DeviceName', str_w)
         if retval != self.myDll.TE_OK:
-            self.statusBar.showMessage("BD Name Write Cache Fail,please retry!!!")
+            #self.statusBar.showMessage("BD Name Write Cache Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">BD Name Write Cache Fail,please retry!!!</font>')
         #step 5 ->teConfigCacheWrite
         retval = self.myDll.teConfigCacheWrite(self.handle1, None, reserved=0)# reserved:Currently unused, should be set to 0.
         if retval != self.myDll.TE_OK:
-            self.statusBar.showMessage("Cache Write Fail,please retry!!!")
+            #self.statusBar.showMessage("Cache Write Fail,please retry!!!")
+            self.statusBar_label.setText('<font color="red">Cache Write Fail,please retry!!!</font>')
         else:
-            self.statusBar.showMessage("Cache Write Succeed")
+            #self.statusBar.showMessage("Cache Write Succeed")
+            self.statusBar_label.setText('<font color="blue">Cache Write Succeed</font>')
 
     def app_psk_read_func(self):
         #print("app_psk_read_func")
@@ -240,20 +270,72 @@ class ChildUiClass(QMainWindow, Ui_MainWindow):
         psk_id = eval(str_value)
         retval, value, readLen = self.myDll.tePsRead(self.handle1, psk_id, 64, value=None)  # 16bit,分层ps 不能用此api 读取，用cacae config
         #print(retval, value, readLen)
+        if retval != self.myDll.TE_OK:
+            self.statusBar_label.setText('<font color="red">App psk id read Fail</font>')
+            return
         show_value = ""
         for index in range(readLen):
             str_index = "".join(f"{value[index]:04x}")
-            print(str_index)
+            #print(str_index)
             show_value += str_index[2:] + " " + str_index[:2] + " "
         #print(show_value.upper().rstrip())
         self.lineEdit_app_psk_value.setText(show_value.upper().rstrip())
+        self.statusBar_label.setText('<font color="blue">App psk id read succeed</font>')
     def app_psk_write_func(self):
-        print("app_psk_write_func")
+        #print("app_psk_write_func")
+        str_value = self.app_psk_id.currentText()
+        if str_value == '':
+            self.statusBar_label.setText('<font color="red">App psk id is NULL</font>')
+            return
+        psk_id = eval(str_value)
+        value_list = self.lineEdit_app_psk_value.text().split(' ')
+        data_len = len(value_list)
+        value=[]
+        for index in range(0, data_len, 2):
+            #print(int(value_list[index], 16) + (int(value_list[index + 1], 16) << 8))
+            value.append(int(value_list[index], 16) + (int(value_list[index + 1], 16) << 8))
+        retval = self.myDll.tePsWrite(self.handle1, psk_id, int(data_len/2), value)
+        if retval != self.myDll.TE_OK:
+            self.statusBar_label.setText('<font color="red">App psk id write Fail</font>')
+        else:
+            self.statusBar_label.setText('<font color="blue">App psk id write Succeed</font>')
     def audio_psk_read_func(self):
-        print("audio_psk_read_func")
+        #print("audio_psk_read_func")
+        str_value = self.audio_psk_id.currentText()
+        if str_value == '':
+            return
+        psk_id = eval(str_value)
+        retval, value, readLen = self.myDll.tePsAudioRead(self.handle1, psk_id, 64, value=None)  # 16bit,分层ps 不能用此api 读取，用cacae config
+        #print(retval, value, readLen)
+        if retval != self.myDll.TE_OK:
+            self.statusBar_label.setText('<font color="red">Audio psk id read Fail</font>')
+            return
+        show_value = ""
+        for index in range(readLen):
+            str_index = "".join(f"{value[index]:04x}")
+            #print(str_index)
+            show_value += str_index[2:] + " " + str_index[:2] + " "
+        #print(show_value.upper().rstrip())
+        self.lineEdit_audio_psk_value.setText(show_value.upper().rstrip())
+        self.statusBar_label.setText('<font color="blue">Audio psk id read succeed</font>')
     def audio_psk_write_func(self):
-        print("audio_psk_write_func")
-
+        #print("audio_psk_write_func")
+        str_value = self.audio_psk_id.currentText()
+        if str_value == '':
+            self.statusBar_label.setText('<font color="red">Audio psk id is NULL</font>')
+            return
+        psk_id = eval(str_value)
+        value_list = self.lineEdit_audio_psk_value.text().split(' ')
+        data_len = len(value_list)
+        value=[]
+        for index in range(0, data_len, 2):
+            #print(int(value_list[index], 16) + (int(value_list[index + 1], 16) << 8))
+            value.append(int(value_list[index], 16) + (int(value_list[index + 1], 16) << 8))
+        retval = self.myDll.tePsAudioWrite(self.handle1, psk_id, int(data_len/2), value)
+        if retval != self.myDll.TE_OK:
+            self.statusBar_label.setText('<font color="red">audio psk id write Fail</font>')
+        else:
+            self.statusBar_label.setText('<font color="blue">audio psk id write Succeed</font>')
 
 
 
--- test.py
+++ test.py
@@ -13,22 +13,27 @@ if __name__ == '__main__':
     _DIRNAME = os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__))
     exe_path = os.path.join(_DIRNAME, "x86", "TestEngine.dll")
     #_dll = ctypes.windll.LoadLibrary(exe_path)
-    # myDll = TestEngine(exe_path)
-    # handle = myDll.openTestEngine(myDll.USBDBG, '104', 0, 5000, 1000)
+    myDll = TestEngine(exe_path)
+    handle = myDll.openTestEngine(myDll.USBDBG, '104', 0, 5000, 1000)
     """
     15 00 FF FF 49 E3 DF 10 66 54 06 F8 74 79 4B 3C C5 9C 7A 21 B8 E1 0F 21 DA 63 4D 76 34 1E C9 0E B7 C5 1B 37 00 00 00 00 01 00 46 1D B1 B8 EE 19 F4 58 4E 06 28 D6 D2 F7 DA 9D 00 00
     """
-    res =[
-        21, 65535, 58185, 4319, 21606, 63494, 31092, 15435, 40133, 8570, 57784, 8463, 25562, 30285, 7732, 3785, 50615, 14107, 0, 0, 1, 7494, 47281, 6638, 22772, 1614, 54824, 63442, 40410, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    len =30
+    # res =[
+    #     21, 65535, 58185, 4319, 21606, 63494, 31092, 15435, 40133, 8570, 57784, 8463, 25562, 30285, 7732, 3785, 50615, 14107, 0, 0, 1, 7494, 47281, 6638, 22772, 1614, 54824, 63442, 40410, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # len =30
+    str = "15 00 FF FF 49 E3 DF 10 66 54 06 F8 74 79 4B 3C C5 9C 7A 21 B8 E1 0F 21 DA 63 4D 76 34 1E C9 0E B7 C5 1B 37 00 00 00 00 01 00 46 1D B1 B8 EE 19 F4 58 4E 06 28 D6 D2 F7 DA 9D 00 00"
+
     show_value=""
-    for index in range(len):
-        str_index = "".join(f"{res[index]:04x}")
-        print(str_index)
-        num1 = str_index[:2]
-        num2 = str_index[2:]
-        show_value+=str_index[2:]+" "+str_index[:2]+" "
-    print(show_value.upper().rstrip())
+    for index in range(0,len(show_value),2):
+        print(index)
+    print(int(len(show_value)/2))
+    # for index in range(len):
+    #     str_index = "".join(f"{res[index]:04x}")
+    #     print(str_index)
+    #     num1 = str_index[:2]
+    #     num2 = str_index[2:]
+    #     show_value+=str_index[2:]+" "+str_index[:2]+" "
+    # print(show_value.upper().rstrip())
     # retval, lap, uap, nap = myDll.psReadBdAddr(handle)
     # print(retval, lap, uap, nap)
     # resetSearch = 1
